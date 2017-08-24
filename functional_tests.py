a4dbf8b993b0e5ab176afe909f5cb04985448fc3
Brandon Wieber <bwieber@shyftanalytics.com>
2017-08-24 15:15:31 -0400

    FT specced out in comments, and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -17,7 +17,6 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         self.fail('Finish the test!')
 
-
         # She is invited to enter a to-do item straight away
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
